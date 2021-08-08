6d893fc7a3b08b3138ec7b3b670f088468c5163f
parvez905 <parvez.jnu.cse.16@gmail.com>
2021-08-09 02:15:09 +0600

    first commit

---
+++ assignment-4.js
@@ -0,0 +1,64 @@
+//problem-1
+/*function seerToMon(seer) {
+    if (typeof seer !='number') {
+       return 'please input integer value!!!' ;
+    }
+    if (seer<1) {
+        return 'please input positive value!!!!';
+        
+    }
+  const  Mon= seer/40;
+  return Mon;
+    
+}
+
+const result=seerToMon(100);
+console.log(result);
+
+*/
+
+//problem-2
+
+/* 
+function totalSales(shirt_quantity,pant_quantity,shoe_quantity) {
+    if (typeof shirt_quantity  !='number') {
+        return 'please input integer value of shirt_quantity';
+        
+    }
+    else if (typeof pant_quantity  !='number') {
+        return 'please input integer value of pant_quantity ';
+        
+    }
+    else if (typeof shoe_quantity  !='number') {
+        return 'please input integer value of shoe_quantity';
+        
+    }
+    if (shirt_quantity<1) {
+        return ' please input positive value of shirt_quantity';
+        
+    }
+   else if (pant_quantity<1) {
+        return ' please input positive value of pant_quantity';
+        
+    }
+    else if (shoe_quantity<1) {
+        return ' please input positive value of shirt_quantity of shoe_quantity';
+        
+    }
+  const  shirt_price=100;
+  const   pant_price=200;
+  const   shoe_price=500;
+ let total_shirt_price=shirt_quantity*shirt_price;
+ let total_pant_price=pant_quantity*pant_price;
+ let total_shoe_price=shoe_quantity*shoe_price;
+
+ totalSales=total_shirt_price+total_pant_price+ total_shoe_price;
+ return totalSales;
+
+    
+}
+
+const result=totalSales(1,2,3);
+console.log(result);
+
+*/
